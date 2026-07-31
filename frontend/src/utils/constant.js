78fca9c0943be3d63fb4696f106b801ed777b62f
Anurag Yadav <ay120006@gmail.com>
2026-08-01 00:40:31 +0530

    Added backend deployed link

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = (window.location.hostname === 'localhost')
+  ? "http://localhost:3000"
+  : "https://assetflow-uu5n.onrender.com";
